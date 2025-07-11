219aabca5769fc4e01bbc1eb14353faed954f934
QUANPD\quanp <quanpd.nkg@gmail.com>
2025-07-11 17:40:51 +0700

    check

--- main.py
+++ main.py
@@ -18,20 +18,30 @@ PREFIX = '645' if VE_SO_TYPE == 'MEGA_6_45' else '655'
 # if VE_SO_TYPE == 'MEGA_6_45':
 #     df = db_utils.load_vietlott_mega_645_data()
 # else:
-df = db_utils.load_vietlott_power_655_data()
+#     df = db_utils.load_vietlott_power_655_data()
+
+df = db_utils.load_vietlott_mega_645_data()
 
 # Xử lý
-X, y = preprocessing.build_training_data(df, max_number=55)
+# X, y = preprocessing.build_training_data(df, max_number=MAX_NUMBER)
+X, y = preprocessing.build_training_data(df, max_number=45)
 
 # Mô hình
-ml_model = model.build_model(input_dim=X.shape[1], output_dim=55)
-ml_model.fit(X, y, epochs=1200, batch_size=16, validation_split=0.2)
+# ml_model = model.build_model(input_dim=X.shape[1], output_dim=MAX_NUMBER)
+ml_model = model.build_model(input_dim=X.shape[1], output_dim=45)
+ml_model.fit(X, y, epochs=1368, batch_size=16, validation_split=0.2)
 
 # Dự đoán
 suggested_numbers = predict.predict_next(ml_model, df)
 
 # In kết quả
-formatted = f'{655} K1'
+# formatted = f'{PREFIX} K1'
+# for d in suggested_numbers:
+#     numbers = ' '.join(f"{n:02d}" for n in d)
+#     formatted += f' S {numbers}'
+# print(formatted)
+
+formatted = f'{645} K1'
 for d in suggested_numbers:
     numbers = ' '.join(f"{n:02d}" for n in d)
     formatted += f' S {numbers}'
